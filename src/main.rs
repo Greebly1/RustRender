7552b4b4e48a6b96a783c5bf3c9107f4b2aeea61
Greebly1 <dakotat103@gmail.com>
2024-06-08 19:55:04 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use glfw::{Window, Context};
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() {
+    let mut glfw_instance = glfw::init(handle_errors).unwrap();
+
+    let (win_width, win_height) : (u32, u32) = (500, 300);
+    let win_title : &str = "Rust3D";
+    let sleep_time : Duration = Duration::new(0, 50000000);
+
+    //glfw_instance.window_hint(glfw::WindowHint::ClientApi(glfw::ClientApiHint::NoApi));
+    //should also inclide window resizable hint
+
+    let (mut window, event_loop) = glfw_instance
+    .create_window(win_width, win_height, win_title, glfw::WindowMode::Windowed).unwrap();
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    while !window.should_close() {
+
+        glfw_instance.poll_events();
+
+        for (_, event) in glfw::flush_messages(&event_loop) {
+            handle_events(&mut window, event);
+        }
+
+        sleep(sleep_time);
+    }
+}
+
+fn handle_events (window: &mut Window, event: glfw::WindowEvent) {
+    match event {
+        glfw::WindowEvent::Close => { window.set_should_close(true) }
+        _ => { }
+    }
+}
+
+fn handle_errors (err: glfw::Error, message : String) {
+    panic!("GLFW failed: {:?}, {:?}", err, message);
+}
